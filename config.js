e445ae8e14c954b594ffedf55c727b47a3be7a7f
Sarvesh Singhrour <ssinghrour@salesforce.com>
2018-05-12 21:09:09 +0530

    Site with sample layout

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    morgan_mode : process.env.MORGEN_MODE || 'dev' // use dev if nothining specified
+}
--- server.js
+++ server.js
@@ -1,12 +1,43 @@
-var express = require('express');
-var app = express();
+const express   = require('express');
+const app       = express();
+const morgan    = require('morgan'); // middleware for color coded logging https://www.npmjs.com/package/morgan
+const config    = require('./config.js');
+const path      = require('path');
 
 const  PORT = process.env.PORT || 8080;
 
-app.get('/*', (req, res) => {
-    res.send('Hello World');
+
+app.use(morgan(config.morgan_mode)); // use dev for now, TODO change it in future
+
+app.use(express.static('temp'));
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+    res.send(req.params); // http://localhost:8080/users/37/books/5, {"userId":"37","bookId":"5"}
 });
+
+app.get('/flights/:from-:to', (req, res) =>{
+    res.send(req.params); // http://localhost:8080/flights/SFO-HYD, {"from":"SFO","to":"HYD"}
+});
+
+app.get('/user/:userId(\d+)', (req, res) => {
+    res.send(req.params);
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/temp/index.html'));
+    //res.send('Hello World');
+});
+
+
 
 app.listen(PORT, () => {
     console.log('Running server at port: ', PORT);
 });
+
+
+
+// useterminal command "nodemon server.js" to start this server on every file save
+
+// use command "mongod" to start mongo database. Default port is usually 27017
+
+// use command  mongo "mongo --host 127.0.0.1:27017" to start a mongo shell
